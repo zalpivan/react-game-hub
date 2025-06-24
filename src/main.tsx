76b759adb388e2e87e38b5105ef324834a76646b
Patrik Ivan <77494302+zalpivan@users.noreply.github.com>
2025-06-24 18:48:03 +0200

    Build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import App from './App'
+import './index.css'
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { Provider } from "./components/ui/provider";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
-  </React.StrictMode>
-);
+    </ChakraProvider>
+  </React.StrictMode>,
+)
