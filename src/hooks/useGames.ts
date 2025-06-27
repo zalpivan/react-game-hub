af3985e6fe4f556a6352e22036f8a98ab4823706
Patrik Ivan <77494302+zalpivan@users.noreply.github.com>
2025-06-27 21:51:01 +0200

    Loading Skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,7 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform}[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
 
@@ -24,22 +24,28 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClient
-      .get<FetchGamesResponse>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results))
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
-        });
+        setLoading(false);
+      });
 
-      return () => controller.abort();
+    return () => controller.abort();
   }, []);
 
-  return {games, error};
-}
+  return { games, error, isLoading };
+};
 
 export default useGames;
