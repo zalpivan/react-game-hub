55e16cf72039b79783c9225fe4b3c996a58f2fd1
Patrik Ivan <77494302+zalpivan@users.noreply.github.com>
2025-07-20 16:08:23 +0200

    Add initial data for genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
+import genres from "../data/genres";
 import apiClient from "../services/api-client";
-import useData, { FetchResponse } from "./useData";
+import { FetchResponse } from "./useData";
 
 export interface Genre {
   id: number;
@@ -14,6 +15,7 @@ const useGenres = () =>
     queryFn: () =>
       apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
     staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: genres.length, results: genres },
   });
 
 export default useGenres;
